68dc8dd788218f38d87f4b25aca2d0170c4215c4
anjali-dange-06 <coderg75@gmail.com>
2026-09-21 23:54:18 +0530

    Add gitignore for Java class files

--- com/demo/Aadi.java
+++ com/demo/Aadi.java
@@ -2,12 +2,9 @@
 //before we used to make many innterfaces but here we no need to make
 package com.demo;
 public interface Aadi<T> {
-<<<<<<< HEAD
-=======
 	
 	
 
->>>>>>> f58c03e (Added Java 8 practice programs)
 void add(T t1,T t2);//type
 //void add(Integer t1,Integer t2)//this method internally made
 //void add(Float t1,Float t2)//this method internally made
